fa59ab3b3faf7a3622eeac26fe79c2c2025bcd75
Abrham Endale <abrhamendale9@gmail.com>
2022-06-30 11:11:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *  main - Entry point
+ *
+ *  description: This is a printf use
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
