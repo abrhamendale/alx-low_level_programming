3d49ccc2a727d7a7cafc42cf5a4f73702be35a77
Abrham Endale <abrhamendale9@gmail.com>
2022-07-05 05:58:42 -0700

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * putchr - Entry point
+ *
+ * Description: Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ **/
+int putchr(void)
+{
+	char st[]  = "_putchar";
+
+	_putchar(st);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * description: Main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchr();
+	return (0);
+}
