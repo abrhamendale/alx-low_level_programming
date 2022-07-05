1413a7186c580383e0ac69d91b39e7fabd853aae
Abrham Endale <abrhamendale9@gmail.com>
2022-07-05 16:26:29 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: Prints every minute
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+
+	while (i < 3)
+	{
+		while (j < 4)
+		{
+			while (k < 6)
+			{
+				while (l < 10)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					write(1, ":", 1);
+					_putchar(k + '0');
+					_putchar(l + '0');
+					write(1, "\n", 1);
+					l++;
+				}
+			l = 0;
+			k++;
+			}
+		l = 0;
+		k = 0;
+		j++;
+		}
+	j = 0;
+	l = 0;
+	k = 0;
+	i++;
+	}
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: Prints every minute
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
