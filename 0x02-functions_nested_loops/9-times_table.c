e1e2d7d5d2134041547e219cc3cd8afb049e76f8
Abrham Endale <abrhamendale9@gmail.com>
2022-07-06 02:26:45 -0700

    Prints the multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * times_table - Entry point
+ *
+ * Description: Prints the time table
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int i = 0;
+	int j = 0;
+	int p = 0;
+
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			p = i * j;
+			if ((p / 10) != 0)
+			_putchar(p / 10 + '0');
+			else
+			write(1, " ", 1);
+			_putchar((p % 10) + '0');
+			if (j < 9)
+			{
+			write(1, ", ", 1);
+			write(1, " ", 1);
+			}
+			j++;
+		}
+	write(1, "\n", 1);
+	j = 0;
+	i++;
+	}
+}
+
