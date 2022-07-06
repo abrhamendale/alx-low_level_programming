55989df458d8bc78e856115a072ba337401a611d
Abrham Endale <abrhamendale9@gmail.com>
2022-07-06 02:42:16 -0700

    Adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: Adds two numbers
+ * @a: Argument
+ * @b: Argument
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
