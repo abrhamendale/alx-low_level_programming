edd51b594d6a0ba94fb9e7597121a4f7e02bc011
Abrham Endale <abrhamendale9@gmail.com>
2022-07-07 11:35:35 -0700

    Prints numbers 0 upto 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: main
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	more_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: Prints numbers 0 upto 9
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+for (j = 0; j < 10 ; j++)
+{
+	for (i = 0; i < 15 ; i++)
+	{
+		if (i > 9)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		else
+		_putchar(i + '0');
+
+	}
+		_putchar('\n');
+}
+_putchar('\n');
+}
