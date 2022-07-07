5ee23271e47d7883f14bd6dac940e9084dc66886
Abrham Endale <abrhamendale9@gmail.com>
2022-07-07 15:22:15 -0700

    Prints number 1 to 100 and Fizz for multiples of 3 and Buzz for multiples of 5.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers 1 upto 100 and fizz buzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+			printf("FizzBuzz ");
+		else if (((i % 3) == 0) && ((i % 5) != 0))
+			printf("Fizz ");
+		else if (((i % 5) == 0) && ((i % 3) != 0))
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
