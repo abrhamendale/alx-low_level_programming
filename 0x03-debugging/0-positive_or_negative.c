1855cee0e01cda80815fc1303b7348656e29a8d9
Abrham Endale <abrhamendale9@gmail.com>
2022-07-07 04:46:53 -0700

    Debugs positive_or_negative function

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * positive_or_negative - Entry point
+ *
+ * desription: Random number generator
+ * @i: Argument
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	int n = i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	printf("%d is zero\n", n);
+	if (n > 0)
+	printf("%d is positive\n", n);
+	if (n < 0)
+	printf("%d is negative\n", n);
+}
+
+
+
