53396fe198518b7d019b80e802f7ba4060e789fc
Abrham Endale <abrhamendale9@gmail.com>
2022-07-11 06:29:55 -0700

    Prints half of a string.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,12 @@ void puts_half(char *s)
 {
 	unsigned long int i;
 
+	if (strlen(s) != 0)
 	for (i = (strlen(s) + 1) / 2 ; i <= strlen(s) - 1 ; i++)
 	{
 		putchar(s[i]);
 	}
+	else
+		putchar(' ');
 	putchar('\n');
 }
