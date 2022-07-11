8e1d08f92dfeaa98760ec1627e1627e6b557cf84
Abrham Endale <abrhamendale9@gmail.com>
2022-07-11 02:48:11 -0700

    Swaps variable values using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ *
+ * Description: swaps the values of two variables
+ * @a: Pointer argument
+ * @b: Pointer argument
+ *
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int i;
+
+      	i = *a;
+	*a = *b;
+	*b = i;
+}
+
