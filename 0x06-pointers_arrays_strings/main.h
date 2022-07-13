fef9e80cd091a8fb2c48ceba40054b2774310d2f
Abrham Endale <abrhamendale9@gmail.com>
2022-07-13 09:30:26 -0700

    Concatenates strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+char *_strcat(char *dest, char *src);
+#endif
