87cdef1eb8d2cefe1139def03a71022d24e5083b
Abrham Endale <abrhamendale9@gmail.com>
2022-07-13 13:46:59 -0700

    Capitalize after non alphanumeric characters.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,11 +18,11 @@ char *cap_string(char *str)
 	{
 		if (str[i] == '\t')
 			str[i] = ' ';
-		if(!isalnum(str[i]))
-		{
-			if (str[i] != '-')
+		if (str[i] == '!' || str[i] == ',' || str[i] == ';' || str[i] == '.'
+				|| str[i] == '?' || str[i] == '"' || str[i] == '('
+				|| str[i] == ')' || str[i] == '{' || str[i] == '}'
+				|| str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
 			str[i + 1] =  toupper(str[i + 1]);
-		}
 	}
 		return (str);
 }
