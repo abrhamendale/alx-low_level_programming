5a41cbb548dd69cc7c50f81e0f3a0ec74924ae7e
Abrham Endale <abrhamendale9@gmail.com>
2022-07-13 11:15:30 -0700

    Reverses an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,7 +12,7 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	char temp;
+	int temp;
 
 	for (i = 0 ; i < (n / 2) ; i++)
 	{
@@ -20,5 +20,4 @@ void reverse_array(int *a, int n)
 		a[i] = a[n - 1 - i];
 		a[n - 1 - i] = temp;
 	}
-
 }
