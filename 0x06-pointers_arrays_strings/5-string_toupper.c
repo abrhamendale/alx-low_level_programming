45273ddcc24effa7cf05a48895a136a8ec8b68f3
Abrham Endale <abrhamendale9@gmail.com>
2022-07-13 11:54:25 -0700

    Changes elements of an array to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,7 +14,7 @@ char *string_toupper(char *str)
 	int size;
 
 	size = sizeof(str) / sizeof(int);
-	for (i = 0 ; i < (size - 1) ; i++)
+	for (i = 0 ; i < size ; i++)
 		str[i] =  toupper(str[i]);
 	return (str);
 }
