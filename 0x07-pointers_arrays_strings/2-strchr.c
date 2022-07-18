09763bdcc3f4d1c67d901c4efc2d120d0693b4f5
Abrham Endale <abrhamendale9@gmail.com>
2022-07-18 10:22:03 -0700

    Locates first occurence of a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ *
+ * Description: Locates a charcter in a string
+ * @s: The string input
+ * @c: The character to be located
+ *
+ * Return: Charcter pointer
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
