a45db08162084b3d22b26cd7be61aee01c3651ee
Abrham Endale <abrhamendale9@gmail.com>
2022-07-18 13:52:37 -0700

    Prints the sums of the diagonal elements of a square array

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Entry point
+ *
+ * Description: Locates a charcter in a string
+ * @a: Character array
+ * @size: Size of the input array
+ *
+ * Return: Nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	unsigned long int sum;
+	int i, j;
+
+	sum = 0;
+	for (i = 0 ; i <= (size * size) ;)
+	{
+		sum += a[i];
+		i = i + size + 1;
+	}
+	printf("%lu, ", sum);
+	sum = 0;
+	for (j = size - 1 ; j <= (size * size) - size ;)
+	{
+		sum += a[j];
+		j = j + size - 1;
+	}
+	printf("%lu\n", sum);
+}
