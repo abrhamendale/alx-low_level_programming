79cce62f0b8b092110d0901893e1b3bb5a7014cf
Abrham Endale <abrhamendale9@gmail.com>
2022-07-18 10:09:15 -0700

    Copies part of an array to another array

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
-#ifndef MAIN_H
-#define MAIN_H
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
