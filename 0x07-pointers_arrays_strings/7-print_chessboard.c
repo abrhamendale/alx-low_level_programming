cc7784ad67c800c2b6aa4ec5e8f225b64c6027b1
Abrham Endale <abrhamendale9@gmail.com>
2022-07-18 12:30:58 -0700

    Prints a chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ *
+ * Description: Locates a charcter in a string
+ * @a: Character array
+ *
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i;
+	int j;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (j = 0 ; j < 8 ; j++)
+		{
+			if (isalpha(a[i][j]))
+				_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
