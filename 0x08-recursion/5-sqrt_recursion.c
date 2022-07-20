6ea4d73fd19e892f3ad9bdb43e79159fb74d8fc4
Abrham Endale <abrhamendale9@gmail.com>
2022-07-20 12:10:09 -0700

    Checks if a given number is prime

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,16 +13,24 @@ int _sqrt_recursion(int n)
 	return (square(n, 1));
 }
 
+/**
+ * square - Function
+ *
+ * Description: Calculates the square of a number
+ * @n: The number whose root is to be calculated
+ * @i: Count
+ *
+ * Return: The square root
+ *
+ */
 int square(int n, int i)
 {
 	if (n == 0)
-	{
 		return (0);
-	}
+	if (n == 1)
+		return (1);
 	if (n < 0 || i == n)
-	{
 		return (-1);
-	}
 	if (i * i == n)
 	{
 		return (i);
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Entry point
+ *
+ * Description: Prints a string
+ * @n: Input number
+ *
+ * Return: Nothing
+ */
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}
+
+/**
+ * prime - Function
+ *
+ * Description: Calculates the square of a number
+ * @n: The number to be checked
+ * @i: Count
+ *
+ * Return: The square root
+ *
+ */
+int prime(int n, int i)
+{
+	if (n == 1 || (-1 * n) == 1)
+		return (0);
+	if (n < 0)
+	{
+		if ((-1 * n) % i == 0 && (n != i))
+			return (0);
+		else
+			return (1);
+	}
+	if ((n % i) == 0 && (n != i))
+		return (0);
+	if (i == n)
+		return (1);
+	return (prime(n, i + 1));
+}
