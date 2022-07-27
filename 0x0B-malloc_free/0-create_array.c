fe1889b001c1852b58ac7ab239e19e5a47db14bc
Abrham Endale <abrhamendale9@gmail.com>
2022-07-27 08:52:20 -0700

    Initializes a buffer using malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,12 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	s = malloc(sizeof(c) * size);
+	if (malloc(sizeof(c) * size == NULL)
+		return (NULL);
+
 	while (i < size)
 	{
 		s[i] = c;
