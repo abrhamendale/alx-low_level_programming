e5bf88dc7d067be342602b8a02680507a5ccf1ae
Abrham Endale <abrhamendale9@gmail.com>
2022-07-27 09:11:42 -0700

    Initializes a buffer using malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	s = malloc(sizeof(c) * size);
-	if (malloc(sizeof(c) * size == NULL)
+	if (malloc(sizeof(c) * size) == NULL)
 		return (NULL);
 
 	while (i < size)
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -28,4 +28,5 @@ int is_palindrome(char *s);
 int palindrome(char *s, unsigned int i);
 int wildcmp(char *s1, char *s2);
 int wild(char *s1, char *s2);
+char *create_array(unsigned int size, char c);
 #endif
