07ded3a4b572f053a1be3020c3ef6ec3bbf294ee
Abrham Endale <abrhamendale9@gmail.com>
2022-08-01 00:29:35 -0700

    Allocates memory using malloc.

---
+++ 0x0c-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ *
+ *
+ * Retur: Created string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j;
+
+	if (malloc(b) == NULL)
+	{
+		exit (98);
+	}
+	return (malloc(b));
+}
