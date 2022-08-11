306149a1296ebe7e58b308d5ac37453175c8f2c8
Abrham Endale <abrhamendale9@gmail.com>
2022-08-11 14:38:58 -0700

    Corrected betty formatting.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,8 @@
  *
  * Return: Nothing
  */
-void print_first()
+void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
