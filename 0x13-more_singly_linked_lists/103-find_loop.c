b1912873df4cef55a860e6a5f6625f363c29f457
Abrham Endale <abrhamendale9@gmail.com>
2022-08-17 07:20:09 -0700

    Finds a loop in a list.

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,11 +3,32 @@
 /**
  * find_listint_loop - Prints all elements of a list
  *
- * @h: Pointer to the list head
+ * @head: Pointer to the list head
  *
  * Return: PThe address of the node where loop starts
  */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *p1, *p2;
+
+	if (!head)
+		return (NULL);
+	p1 = p2 = head;
+	while (p2->next && p2->next->next)
+	{
+		p2 = p2->next->next;
+		p1 = p1->next;
+		if (p1 == p2)
+		{
+			p1 = head;
+			while (p1 != p2)
+			{
+				p1 = p1->next;
+				p2 = p2->next;
+			}
+			return (p2);
+		}
+	}
+	return (NULL);
 }
 
