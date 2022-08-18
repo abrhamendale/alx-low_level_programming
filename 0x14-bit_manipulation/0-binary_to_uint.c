cfb78266d0d0564adbfb6592b1040ea0a50d10f3
Abrham Endale <abrhamendale9@gmail.com>
2022-08-18 13:35:10 -0700

    Replaced all printfs with putchar.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ie = 0, m = 1;
 	int k = (int)strlen(b), i;
-	
+
 	for (i = k - 1 ; i >= 0 ; i--)
 	{
 		if ((unsigned int)b[i] < 48 || (unsigned int)b[i] > 58)
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,7 +12,7 @@ void print_binary(unsigned long int n)
 	int c, binry, z = 0;
 
 	if (n == 0)
-		printf("0");
+		_putchar("0");
 	for (c = 31 ; c >= 0 ; c--)
 	{
 		binry = n >> c;
