4b373fa7bb660a393d34d08650391ed9f9dcea33
Abrham Endale <abrhamendale9@gmail.com>
2022-08-23 10:52:54 -0700

    Corrected betty style for 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,24 +15,29 @@ int  main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n")
+			, exit(97);
 	f_fd = open(argv[1], O_RDONLY);
 	if (f_fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	t_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
+				, argv[1]), exit(98);
+	t_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSION);
 	if (t_fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((rcount = read(f_fd, buffer, 1024)) > 0)
 	{
 		if (write(t_fd, buffer, rcount) != rcount)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n"
+					, argv[2]), exit(99);
 	}
 	if (rcount == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
+				, argv[1]), exit(98);
 	f_fd = close(f_fd);
 	t_fd = close(t_fd);
 	if (f_fd)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_fd), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_fd)
+			, exit(100);
 	if (t_fd)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_fd), exit(100);
 	return (EXIT_SUCCESS);
