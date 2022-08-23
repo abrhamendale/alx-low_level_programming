867fbf6131bc0f1112a0b9515676364f8d8f6ea4
Abrham Endale <abrhamendale9@gmail.com>
2022-08-23 10:48:41 -0700

    Cop added 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,39 @@
 #include "main.h"
 
 /**
- * read_textfile - Outputs the contents of a text file
+ * main - copies content from one file to another
  *
- * @filename: The text file name
- * @letters: Number of letters to read
+ * @argc: Number of arguments
+ * @argv: Vector of arguments
  *
- * Return: Actual number of letters read
+ * Return: Nothing
  */
-ssize_t cp(const char *file_from, const char *file_to)
+int  main(int argc, char *argv[])
 {
+	int f_fd = 0, t_fd = 0;
+	ssize_t rcount;
+	char buffer[1024];
 
-}
-void main (int argc, char *argv[]) 
-{
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	f_fd = open(argv[1], O_RDONLY);
+	if (f_fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	t_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (t_fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((rcount = read(f_fd, buffer, 1024)) > 0)
+	{
+		if (write(t_fd, buffer, rcount) != rcount)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	if (rcount == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	f_fd = close(f_fd);
+	t_fd = close(t_fd);
+	if (f_fd)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_fd), exit(100);
+	if (t_fd)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_fd), exit(100);
+	return (EXIT_SUCCESS);
 }
