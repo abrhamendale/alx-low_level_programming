9e18c0720b3ed0cf706ecc98642bc469e30f1e7b
Abrham Endale <tmailae2020@gmail.com>
2022-09-29 19:56:23 -0700

    Fixed free list.

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -12,19 +12,20 @@ void free_dlistint(dlistint_t *head)
 	dlistint_t *p1, *p2;
 
 	if (head == NULL)
-		return (NULL);
-	p1 = p2 = head;
-	while (p1->next != NULL)
 	{
-		p2 = p1;
-		p1 = p1->next;
+		p1 = p2 = head;
+		while (p1->next != NULL)
+		{
+			p2 = p1;
+			p1 = p1->next;
+		}
+		while (p2->prev != NULL)
+		{
+			free(p1);
+			p1 = p2;
+			p2 = p2->prev;
+		}
+		free(p2->next);
+		free(p2);
 	}
-	while (p2->prev != NULL)
-	{
-		free(p1);
-		p1 = p2;
-		p2 = p2->prev;
-	}
-	free(p2->next);
-	free(p2);
 }
