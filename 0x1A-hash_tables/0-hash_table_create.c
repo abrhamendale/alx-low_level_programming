4818fc9f51a45667cfe615894e3000df37502098
Abrham Endale <tmailae2020@gmail.com>
2022-10-21 21:42:57 -0700

    Fixed malloc return.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table 
+ * hash_table_create - Creates a hash table
  *
  * @size: The size of the hash table
  *
@@ -10,27 +10,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *first;
-	/*hash_node_t *head, *parser;
-	unsigned int counter = 0;*/
 
-	printf("hash_table_create--------\n");
 	first = malloc(sizeof(hash_table_t));
-	first->array = malloc(sizeof(hash_node_t **) * size);
-	/*head = NULL;
-	if (head == NULL)
-	{
-		head = malloc(sizeof(hash_node_t) * size);
-		head->next = NULL;
-		counter++;
-	}
-	while(counter <= size)
-	{
-		parser = malloc(sizeof(hash_node_t));
-		parser->next = head;
-		head = parser;
-		counter++;
-	}
-	first->array = &head;*/
+	if (first == NULL)
+		return (NULL);
+	first->size = size;
+	first->array = malloc(sizeof(hash_node_t *) * first->size);
+	if (first->array == NULL)
+		return (NULL);
 	return (first);
 }
 
