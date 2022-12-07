cdfe2ad9b79fb88f8985fddafc2a632c3d66682c
Abrham Endale <tmailae2020@gmail.com>
2022-12-07 13:21:52 -0800

    Added perimeter calcuation python code.

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Computes the perimeter of an island in a list of lists.
+    """
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i - 1 >= 0 and grid[i - 1][j] == 0:
+                    p = p + 1
+                if i + 1 < len(grid) and grid[i + 1][j] == 0:
+                    p = p + 1
+                if j - 1 >= 0 and grid[i][j - 1] == 0:
+                    p = p + 1
+                if j + 1 < len(grid[i]) and grid[i][j + 1] == 0:
+                    p = p + 1
+            j = j + 1
+        i = i + 1
+    return (p)
