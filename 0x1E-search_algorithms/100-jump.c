cc06e2893bafbbe79f201768cd07a4b3a1ab6adc
Abrham Endale <tmailae2020@gmail.com>
2023-01-29 14:49:15 -0800

    Fixed more errors.

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,7 +29,7 @@ int jump_search(int *array, size_t size, int value)
 	while (jump <= i)
 	{
 		printf("Value checked array[%lu] = [%d]\n", jump, array[jump]);
-		if (array[j] == value)
+		if (array[jump] == value)
 			return (jump);
 		jump++;
 	}
