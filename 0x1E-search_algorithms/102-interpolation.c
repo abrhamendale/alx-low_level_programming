96acb23d0bd951f0343f5827013423921ada29ac
Abrham Endale <tmailae2020@gmail.com>
2023-01-29 23:06:57 -0800

    More fixes.

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Implements an interpolation search algorithm
+ * 
+ * @array: Input array
+ * @size: The size of the array
+ * @value: value to match element
+ *
+ * Return: -The index of the element
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	unsigned int l = 0, r = size - 1, parser;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	while (array[r] != array[l])
+	{
+		parser = l + (((double)(r - l) / (array[r] - array[l]))
+			       * (value - array[l]));
+
+		if (parser > size)
+		{
+			printf("Value checked array[%d] is out of range\n"
+					, parser);
+			return (-1);
+		}
+		printf("Value checked array[%d] = [%d]\n", parser, array[parser]);
+		if (value > array[parser])
+			l = parser + 1;
+		else if (value < array[parser])
+			r = parser - 1;
+		else
+			return (parser);
+	}
+	if (value == array[l])
+		return (l);
+	else
+		return (-1);
+}
---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,84 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - Implements the exponential search
+ *
+ * @array: Input array
+ * @size: Size of the array
+ * @value: The value to be searched
+ *
+ * Return: The index of the number to be searched
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t bound = 1;
+
+	if (size == 0)
+		return (-1);
+
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
+
+	if (bound + 1 < size)
+		return (bin(array, (bound / 2) - 1, bound + 1, value));
+	else
+		return (bin(array, (bound / 2) - 1, size, value));
+}
+/**
+ * binary_search - Searches for a value using binary search algorithm
+ *
+ * @array: Input array
+ * @size: Size of the array
+ * @value: Value to look for
+ *
+ * Return: The first index where value is stored
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int ret = 0;
+
+	ret = bin(array, -1, size, value);
+	if (ret > 0)
+		return (ret);
+	return (-1);
+}
+/**
+ * bin - Helper function for binary search
+ *
+ * @array: The input array
+ * @l: Start of the array
+ * @r: End of the list
+ * @value: Value to be searched
+ *
+ * Return: The index
+ */
+int bin(int *array, size_t l, size_t r, int value)
+{
+	size_t i = 0, m = (r + l) / 2;
+	int ret = 0;
+
+	if (l + 1 == r)
+		return (-1);
+	printf("Searching in array: ");
+	for (i = l + 1 ; i < r ; i++)
+	{
+		printf("%d", array[i]);
+		if (i + 1 < r)
+			printf(", ");
+	}
+	printf("\n");
+	if (array[m] == value)
+		return (m);
+	if (value > array[m])
+	{
+		ret = bin(array, m, r, value);
+	}
+	else
+	{
+		ret = bin(array, l, m, value);
+	}
+	return (ret);
+}
